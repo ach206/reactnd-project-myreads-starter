addd85a714631a50eedb814445d82b434503d89d
Camille Hall <camille@creativeambition.net>
2018-10-19 17:15:40 -0700

    shelf is filtered and renders

--- src/comps/Shelf.js
+++ src/comps/Shelf.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import Book from './Book'
+// import * as BooksAPI from '.././BooksAPI'
+
 
 class Shelf extends Component {
+
   render() {
     const shelfName = this.props.name
+    const booksData = this.props.category
+    const listItems = booksData.map((book) =>
+      <li key={book.id}>
+        <Book data={booksData} />
+      </li>
+    );
         return (
             <div className="bookshelf">
             <h2 className="bookshelf-title">{shelfName}</h2>
             <div className="bookshelf-books">
               <ol className="books-grid">
-                <Book />
+              {listItems}
               </ol>
             </div>
           </div>
